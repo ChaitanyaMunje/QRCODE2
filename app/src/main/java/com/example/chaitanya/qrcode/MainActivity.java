894daee6456d340e3688abb22e779acae13fbb84
Chaitanya <chaitanyamunje@gmail.com>
2018-06-14 15:45:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chaitanya/qrcode/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.chaitanya.qrcode;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txt;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt=(TextView)findViewById(R.id.text);
+        btn=(Button)findViewById(R.id.button);
+        final Activity activity=this;
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator=new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("SCAN");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBeepEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled Scanning", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            } } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        } }}
